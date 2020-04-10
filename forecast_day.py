3ba7204c5693fdb3aa4df63c19b72a7f034815fa
ah-aydin <ah-aydin@users.noreply.github.com>
2020-04-10 17:37:19 +0300

    Update forecast_day.py

--- forecast_day.py
+++ forecast_day.py
@@ -1,20 +1,20 @@
 class ForecastDay():
     def __init__(self, day_of_the_week, day_of_the_month, month, image, temperature, weather, feels_like, wind, humidity, p_chance, p_ammount, UV, sunrise, sunset):
-        self.day_of_the_week = day_of_the_week[0].upper() + day_of_the_week[1:]
-        self.day_of_the_month = day_of_the_month
-        self.month = month
+        self.day_of_the_week = day_of_the_week.upper()
+        self.day_of_the_month = day_of_the_month.upper()
+        self.month = month.upper()
         self.image = image
-        self.temperature = temperature
-        self.weather = ' ' + weather
-        self.feels_like = feels_like
-        self.wind = wind
-        self.humidity = humidity
-        self.p_chance = p_chance
-        self.p_ammount = p_ammount
-        self.UV = UV
-        self.sunrise = sunrise
-        self.sunset = sunset
-    
+        self.temperature = temperature.upper()
+        self.weather = ' ' + weather.upper()
+        self.feels_like = feels_like.upper()
+        self.wind = wind.upper()
+        self.humidity = humidity.upper()
+        self.p_chance = p_chance.upper()
+        self.p_ammount = p_ammount.upper()
+        self.UV = UV.upper()
+        self.sunrise = sunrise.upper()
+        self.sunset = sunset.upper()
+
     def __str__(self):
         return ' '.join([self.day_of_the_week, self.day_of_the_month, self.month, self.temperature, self.weather, self.feels_like, 
                         self.wind, self.humidity, self.p_chance, self.p_ammount, self.UV, self.sunrise, self.sunset])
